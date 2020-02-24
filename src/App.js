47075be211cce27ccce2914f82030917fc4fb4e3
kanji <kanjigodhani00@gmail.com>
2020-02-24 19:57:15 +0530

    Design, route change

--- src/App.js
+++ src/App.js
@@ -52,7 +52,7 @@ class App extends Component {
             <Route path={'/edit-app/:id'} component={EditApp}/>
             <Route path={'/create-apps'} component={CreateApp}/>
             <Route path={'/review-apps'} component={ReviewApps}/>
-            <Route path={'/grant-access:app?'} component={GrantAccess}/>
+            <Route path={'/grant-access/:app?'} component={GrantAccess}/>
             <Route path={'/revoke-access/:app?'} component={RevokeAccess}/>
             <Route path={'/role-manage/:id'} component={RoleManagement}/>
             <Route path={'/'} component={AppsList}/>
