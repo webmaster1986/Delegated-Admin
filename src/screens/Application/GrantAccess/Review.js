44ac3db6636da2c659fe75343fc42aef571b4ebe
kanji <kanjigodhani00@gmail.com>
2020-02-24 17:54:32 +0530

    modal change

--- src/screens/Application/GrantAccess/Review.js
+++ src/screens/Application/GrantAccess/Review.js
@@ -1,10 +1,65 @@
-import React from "react";
-import {Button, Col, Form, InputGroup, Row} from "react-bootstrap";
+import React, { useState } from "react";
+import {Button, Col, Row} from "react-bootstrap";
 import { Table } from 'antd';
+import Modal from "../Modal";
 
 const Review = (props) => {
+    const [modalData, setModalData] = useState(null);
+    const [visible, setVisible] = useState(null);
+
+    const handelModal = (e, record, sub, rootRecord) => {
+        const {data, category} = props
+        let result = {}
+        if (sub) {
+            if (sub === "user") {
+                const selectedRecord = rootRecord && rootRecord.users.find(g => g.login === record) || {}
+                result = {
+                    login: selectedRecord.login,
+                    name: selectedRecord.name,
+                    email: selectedRecord.email,
+                    bureau: selectedRecord.bureau
+                }
+            } else {
+                const selectedRecord = rootRecord && rootRecord.roles.find(g => g.appCode === record) || {}
+                result = {
+                    appCode: selectedRecord.appCode,
+                    oimTarget: selectedRecord.oimTarget,
+                    roleName: selectedRecord.roleName,
+                    roleDescription: selectedRecord.roleDescription
+                }
+            }
+        } else {
+            if (record) {
+                if (category === "byUser") {
+                    const selectedRecord = data.find(g => g.login === record) || {}
+                    result = {
+                        login: selectedRecord.login,
+                        name: selectedRecord.name,
+                        email: selectedRecord.email,
+                        bureau: selectedRecord.bureau
+                    }
+                } else {
+                    const selectedRecord = data.find(g => g.roleName === record) || {}
+                    result = {
+                        appCode: selectedRecord.appCode,
+                        oimTarget: selectedRecord.oimTarget,
+                        roleName: selectedRecord.roleName,
+                        roleDescription: selectedRecord.roleDescription
+                    }
+                }
+            }
+        }
+
+        setModalData(result)
+        setVisible(!visible)
+    }
+
     const columnsByUser = [
-        { title: 'User Login', dataIndex: 'login', key: 'login' },
+        { title: 'User Login',
+          dataIndex: 'login',
+          key: 'login',
+          render: (record) => <div className="link-text" onClick={(e) => handelModal(e, record)}><u>{record}</u></div>
+        },
         { title: 'User Name', dataIndex: 'name', key: 'name' },
         { title: 'Email', dataIndex: 'email', key: 'email' },
         { title: 'Bureau', dataIndex: 'bureau', key: 'bureau' },
@@ -17,7 +72,11 @@ const Review = (props) => {
     ];
 
     const columnsByRole = [
-        { title: 'Role Name', dataIndex: 'roleName', key: 'name' },
+        { title: 'Role Name',
+          dataIndex: 'roleName',
+          key: 'name',
+          render: (record) => <div className="link-text" onClick={(e) => handelModal(e, record)}><u>{record}</u></div>
+        },
         { title: 'App Code', dataIndex: 'appCode', key: 'age' },
         { title: 'Role Description', dataIndex: 'roleDescription', key: 'roleDescription' },
         { title: 'OIM Target', dataIndex: 'oimTarget', key: 'oimTarget' },
@@ -32,7 +91,11 @@ const Review = (props) => {
     const userColumn = (rootRecord) => {
         return (
             [
-                { title: 'Login', dataIndex: 'login', key: 'login' },
+                { title: 'Login',
+                  dataIndex: 'login',
+                  key: 'login',
+                  render: (record) => <div className="link-text" onClick={(e) => handelModal(e, record, "user", rootRecord)}><u>{record}</u></div>
+                },
                 { title: 'Name', dataIndex: 'name', key: 'name' },
                 { title: 'Email', dataIndex: 'email', key: 'email' },
                 { title: 'Bureau', dataIndex: 'bureau', key: 'bureau' },
@@ -49,7 +112,11 @@ const Review = (props) => {
     const tagColumn = (rootRecord) => {
         return (
             [
-                { title: 'App Code', dataIndex: 'appCode', key: 'appCode' },
+                { title: 'App Code',
+                  dataIndex: 'appCode',
+                  key: 'appCode',
+                  render: (record) => <div className="link-text" onClick={(e) => handelModal(e, record, "role", rootRecord)}><u>{record}</u></div>
+                },
                 { title: 'Role Name', dataIndex: 'roleName', key: 'roleName' },
                 { title: 'Role Description', dataIndex: 'roleDescription', key: 'roleDescription' },
                 { title: 'OIM Target', dataIndex: 'oimTarget', key: 'oimTarget' },
@@ -65,19 +132,7 @@ const Review = (props) => {
 
     return (
         <>
-            {/* <Row className={'mb-3'}>
-                <Col>
-                    <InputGroup>
-                        <Form.Control
-                            type="text"
-                            placeholder="Search..."
-                            aria-describedby="inputGroupPrepend"
-                            name="username"
-                        />
-                    </InputGroup>
-                </Col>
-            </Row> */}
-
+            <Modal visible={visible} data={modalData} handelModal={handelModal} title={props.category === "byUser" ? "User Data" : "Role Data"}/>
             <Table
                 columns={props.category === "byRole" ? columnsByRole : columnsByUser}
                 dataSource={props && props.data}
